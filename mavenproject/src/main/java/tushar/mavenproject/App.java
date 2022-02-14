90a99763135e8cdb076889c9a4eb9e8a32eb1b16
tushar.patil <tushar.patil@inbl01lap7248.brd.g-c-i.com>
2022-02-14 22:23:38 +0530

    commit

---
+++ mavenproject/src/main/java/tushar/mavenproject/App.java
@@ -0,0 +1,14 @@
+package tushar.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.err.println("This is Maven ");
+    }
+}
